69450b4b293430211858a1515f0efc288a97093a
jasonhuangjin8 <77730733+jasonhuangjin8@users.noreply.github.com>
2022-06-02 14:32:54 +0800

    Update duizhang.py
    change

--- jd/duizhang.py
+++ jd/duizhang.py
@@ -16,7 +16,7 @@ def main():
             if "对账" in file:
                 print(file)
                 df = pandas.read_excel(file, sheet_name='团购结账明细表',header=3,skipfooter=1,usecols=['预订账号', '抵店时间','结账时间','姓名','房号'])
-                conn=pymysql.connect(host='10.200.1.100', port=3306, user='huang', passwd='huang520', db='orderid', charset='utf8')
+                conn=pymysql.connect(host='10.200.1.100', port=3306, user='pwd', passwd='pwd', db='orderid', charset='utf8')
 
                 cur=conn.cursor()#获取游标
                 #另一种插入数据的方式，通过字符串传入值
